1735e5d6b55ea51e2facc1f5fd159c9a1c9dc7ea
LaCajaVengadora <oviedo.cerdeira.lorenzo@gmail.com>
2026-07-20 14:18:54 -0300

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sachiel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
